3eceafb7eabc7e30fe88de619bd2ff026881fcf6
Ümit Aslan <prototprotot2@gmail.com>
2022-05-08 18:01:52 +0300

    scoreText creating

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class BirdController : MonoBehaviour
 {
     Rigidbody2D rb;
+    Game_Manager game_Manager;
+    
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        game_Manager=FindObjectOfType<Game_Manager>();
     }
-
-    
     void Update() 
     {
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity=Vector2.up*Time.deltaTime*270;
             
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("score"))
+        {
+            game_Manager.scoreText.text+=1.ToString();
         }
     }
 }
---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game_Manager : MonoBehaviour
+{
+ 
+    public TextMeshProUGUI scoreText;
+    public int score;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
