1cc656230e8c54db59c699f04d5f98d2039ba8ac
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-05-23 22:55:13 +0300

    Comment lines added

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -11,18 +11,18 @@ public class BirdController : MonoBehaviour
     {
         Time.timeScale=0;
         rb=GetComponent<Rigidbody2D>();
-        game_Manager=FindObjectOfType<Game_Manager>();
+        game_Manager=FindObjectOfType<Game_Manager>();//find the game manager
     }
     void Update() 
     {
         if (Input.GetMouseButtonDown(0)&&game_Manager.gameOver==false)
         {
-            game_Manager.gameStart=true;
-            Time.timeScale=1;
-            game_Manager.gameReadyPanel.SetActive(false);
-            rb.velocity=Vector2.zero;
-            rb.velocity=Vector2.up*velocity;
-            game_Manager.audioSource.PlayOneShot(game_Manager.audioClips[0]);
+            game_Manager.gameStart=true;//start the game
+            Time.timeScale=1;//set the time scale to 1
+            game_Manager.gameReadyPanel.SetActive(false);//hide the game ready panel
+            rb.velocity=Vector2.zero;//set the velocity to zero
+            rb.velocity=Vector2.up*velocity;//set the velocity to up
+            game_Manager.audioSource.PlayOneShot(game_Manager.audioClips[0]);//play the audio clip
             
         }
      
