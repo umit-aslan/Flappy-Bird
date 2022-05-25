f42b3768892f21b9382b36aa2c3bff3e96e45467
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-05-25 20:42:02 +0300

    Comment lines added

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -6,9 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour
 {
-    public TextMeshProUGUI scoreText;
-    public int score=0;
-    public AudioSource audioSource;
+    public TextMeshProUGUI scoreText;//score text
+    public int score=0;//score of the player
+    public AudioSource audioSource;//audio source for the game manager
     public AudioClip[] audioClips;
     public GameObject gameoverPanel;
     public bool gameStart=false;
@@ -16,10 +16,10 @@ public class Game_Manager : MonoBehaviour
     public bool gameOver=false;
     void Start()
     {
-        audioSource=GetComponent<AudioSource>();
+        audioSource=GetComponent<AudioSource>();//Get the audio source component
     }
     public void Restart()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0);//Load the first scene
     }
 }
