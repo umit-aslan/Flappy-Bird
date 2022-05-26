b6c673cabc427458a91fddc74cb8da08c315904a
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-05-26 20:47:20 +0300

    Comment lines added

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -9,11 +9,11 @@ public class Game_Manager : MonoBehaviour
     public TextMeshProUGUI scoreText;//score text
     public int score=0;//score of the player
     public AudioSource audioSource;//audio source for the game manager
-    public AudioClip[] audioClips;
-    public GameObject gameoverPanel;
-    public bool gameStart=false;
-    public GameObject gameReadyPanel;
-    public bool gameOver=false;
+    public AudioClip[] audioClips;//audio clips for the game manager
+    public GameObject gameoverPanel;//game over panel
+    public bool gameStart=false;//game start
+    public GameObject gameReadyPanel;//game ready panel
+    public bool gameOver=false;//game over
     void Start()
     {
         audioSource=GetComponent<AudioSource>();//Get the audio source component
