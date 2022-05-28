92d62d1958c8307e8f2eec8b9bdf333552b45eb0
Ümit Aslan <79724156+umit-aslan@users.noreply.github.com>
2022-05-28 10:53:24 +0300

    Comment lines added

--- Assets/Scripts/PipeSpawn.cs
+++ Assets/Scripts/PipeSpawn.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class PipeSpawn : MonoBehaviour
 {
-    public GameObject pipe;
+    public GameObject pipe;//pipe prefab
     void Start()
     {
-        StartCoroutine(Spawn());
+        StartCoroutine(Spawn());//Start the coroutine
     }
     IEnumerator Spawn()
     {
         while (true)
         {
-        Vector3 poz=new Vector2(transform.position.x,Random.Range(-5f,-1f));
-        GameObject PipeObject=Instantiate(pipe,poz,Quaternion.identity);
-        yield return new WaitForSeconds(1.7f);
-        Destroy(PipeObject,5f);
+        Vector3 poz=new Vector2(transform.position.x,Random.Range(-5f,-1f));//Random position for the pipe
+        GameObject PipeObject=Instantiate(pipe,poz,Quaternion.identity);//Instantiate the pipe
+        yield return new WaitForSeconds(1.8f);//Wait for 1.8 seconds
+        Destroy(PipeObject,5f);//Destroy the pipe after 5 seconds
         }
     }
 }
